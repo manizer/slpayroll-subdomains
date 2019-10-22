10380343258f7dac2e41ca2b3f2e347e089bf7be
Fernando Christyanto <christyantofernando@gmail.com>
2019-10-22 16:29:31 +0700

    Add subdomains and data

---
+++ Data/DBData/Lookup.cs
@@ -0,0 +1,20 @@
+public static class Lookup{
+    public static class Religion{
+        public const int KATHOLIK = 1;
+        public const int KRISTEN = 2;
+        public const int ISLAM = 3;
+        public const int BUDDHA = 4;
+        public const int HINDU = 5;
+        public const int DLL = 6;
+    }
+    public static class DebtInstallmentStatus {
+        public const int BELUM_DIBAYAR = 1;
+        public const int TELAH_DIBAYAR = 2;
+        public const int DIANGGAP_LUNAS = 3;
+    }
+
+    public static class DebtStatus{
+        public const int BELUM_LUNAS = 1;
+        public const int LUNAS = 2;
+    }
+}
