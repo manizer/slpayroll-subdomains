e57a0fc3658f2c076ecdf5f3ef7953bf63811d78
Fernando Christyanto <christyantofernando@gmail.com>
2019-10-29 13:31:09 +0700

    Remove UserSubdomain

--- Model/Subdomains/UserProfileSubdomain/UserProfile.cs
+++ Model/Subdomains/UserProfileSubdomain/UserProfile.cs
@@ -1,9 +1,13 @@
 namespace Model.Subdomains.UserProfileSubdomain{
-    class UserProfile{
+    public class UserProfile{
+        public int ID;
         public string PhotoURL;
+        public string FullName;
         public int SchoolID;
         public int RoleID;
+        public string NIK;
         public string Email;
         public string PhoneNumber;
+        public DateTime LastLogin;
     }
 }
--- Model/Subdomains/UserSubdomain/School.cs
+++
@@ -1,8 +0,0 @@
-namespace Model.Subdomains.UserSubdomain{
-    class School: MasterData{
-        public int ID;
-        public int Name;
-        public int ID() => this.ID;
-        public string Name() => this.Name;
-    }
-}
