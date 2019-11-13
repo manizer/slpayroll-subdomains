db3b41263af5f3a0cc785060285e0c09a5b28d17
Yohanzen Christanto Alexander <yohanzen.alexander@gmail.com>
2019-11-13 13:47:49 +0700

    Add dashboard subdomain

---
+++ Model/Subdomains/DashboardSubdomain/ChartData.cs
@@ -0,0 +1,7 @@
+namespace Model.Subdomains.DashboardSubdomain{
+    public class ChartData{
+        public string CategoryName;
+        public int Employee { get; set; }
+        public int Teacher { get; set; }
+    }
+}
---
+++ Model/Subdomains/DashboardSubdomain/School.cs
@@ -0,0 +1,6 @@
+namespace Model.Subdomains.DashboardSubdomain{
+    public class School{
+        public int ID;
+        public string Name;
+    }
+}
