e1137ec446e82fdb6b375a7e5cd26d67c154fe67
Fernando Christyanto <christyantofernando@gmail.com>
2019-11-14 14:03:47 +0700

    Resolve #12, add ServiceYearAllowance

---
+++ Model/Subdomains/AllowanceSubdomain/Permanent/ServiceYearAllowance.cs
@@ -0,0 +1,11 @@
+namespace Model.Subdomains.AllowanceSubdomain.Permanent{
+    public class ServiceYearAllowance{
+        public int ID;
+        public int SchoolID;
+        public int StartMonth;
+        public int StartYear;
+        public int EndMonth;
+        public int EndYear;
+        public int Value;
+    }
+}
--- Model/Subdomains/AllowanceSubdomain/Permanent/SpecialAllowance.cs
+++ Model/Subdomains/AllowanceSubdomain/Permanent/SpecialAllowance.cs
@@ -2,7 +2,7 @@ namespace Model.Subdomains.AllowanceSubdomain.Permanent{
     public class SpecialAllowance{
         public List<AllowanceDetail> SpecialAllowances;
         public List<KaryaAllowance> KaryaAllowances;
-        // masa kerja
+        public List<ServiceYearAllowance> ServiceYearAllowances;
         public List<AdditionalAllowance> AdditionalAllowances;
     }
 }
