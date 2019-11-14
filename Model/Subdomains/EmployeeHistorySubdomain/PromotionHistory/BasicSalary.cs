60eff0d10edff64a29a9c3b86ff8fb2f098e1323
Fernando Christyanto <christyantofernando@gmail.com>
2019-11-14 16:28:41 +0700

    Add basicsalary to employeepromotion

---
+++ Model/Subdomains/EmployeeHistorySubdomain/PromotionHistory/BasicSalary.cs
@@ -0,0 +1,9 @@
+namespace Model.Subdomains.EmployeeHistorySubdomain.PromotionHistory {
+    public class BasicSalary{
+        public int ID;
+        public int SchoolID;
+        public int GradeID;
+        public int WorkPeriod;
+        public int Value;
+    }
+}
