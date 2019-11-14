dab4eeefe1865e446caa78c342c0be09233c4605
Fernando Christyanto <christyantofernando@gmail.com>
2019-11-14 13:58:13 +0700

    Resolve #12, add special allowance & allowance honorer

---
+++ Model/Subdomains/AllowanceSubdomain/Honorer/Allowance.cs
@@ -0,0 +1,8 @@
+namespace Model.Subdomains.AllowanceSubdomain.Honorer{
+    public class Allowance{
+        public List<AllowanceDetail> MonthlyAllowances;
+        public List<PositionalAllowance> PositionalAllowance;
+        public List<YearlyAllowance> YearlyAllowance;
+        public List<AdditionalAllowance> AdditionalAllowances;
+    }
+}
